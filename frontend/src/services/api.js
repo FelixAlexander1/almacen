afa3989bf16facf29f86628d2d833ab692c9107c
FelixAlexander1 <alemongar022@gmail.com>
2025-09-22 23:42:57 +0100

    Descripci��n de cambios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,21 @@
 // src/services/api.js
 import axios from 'axios';
 
-const api = axios.create({
+/*const api = axios.create({
   baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
+});*/
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://almacen-1-ezuu.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+
 //Actividades recientes
 export const getLatestActivities = () => api.get('/activities/latest');
 
